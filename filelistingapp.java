e421e586f8b585fae666170cce5f878c2519c320
anniescott701 <anniescott701@gmail.com>
2018-02-04 19:07:26 +0530

    adding annie.txt and filelistingapp.java files

--- filelistingapp.java
+++ filelistingapp.java
@@ -1,10 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class filelistingapp {
+	public static ArrayList<String> files = new ArrayList<String>();
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the file name in this project: ");
@@ -12,21 +17,24 @@ public class filelistingapp {
 		url += in.next();
 
 		BufferedReader br = new BufferedReader(new FileReader(url));
+		String line;
+
 		try {
-			String line = "";
-			System.out.print("\n-----Reading File Contents-----\n");
-			while ((line = br.readLine()) != null) {
-				System.out.println("\n" + line);
-				if (line.endsWith(".csv")) {
-					System.out.println("**CSV Identified, Reading file**");
-					readFile(line);
-				} else if (line.endsWith("\\")) {
-					System.out.println("**URL Identified, Fetching files and folders**");
-					getList(line);
-				} else {
-					System.out.println("**Invalid Path**");
-				}
-			}
+
+			line = br.readLine();
+			System.out.print("\nReading file names and folder contenets\n");
+			System.out.println(line);
+			getList(line);
+
+			// System.out.println(arrayData);
+
+			line = br.readLine();
+			System.out.print("\nWriting the contents to the CSV\n");
+			System.out.println(line);
+			writeCSV(line);
+
+			System.out.print("\nWriting Complete\n");
+
 			br.close();
 		} catch (Exception ex) {
 			System.out.println(ex + " Exception Caught");
@@ -37,16 +45,31 @@ public class filelistingapp {
 	}
 
 	public static void getList(String dir) {
+
 		File list[] = new File(dir).listFiles();
 		for (File file : list)
-			if(file.isFile())
-			{
-			System.out.println("filename"+file.getName()+"-----------------filepath:"+file.getAbsolutePath());
-			}
-			else
-			{
+			if (file.isFile()) {
+				files.add(file + "");
+			} else {
 				getList(file.getAbsolutePath());
 			}
+	}
+
+	public static void writeCSV(String dir) throws Exception {
+		Iterator<String> itr = files.iterator();
+		FileWriter csv = new FileWriter(dir);
+		BufferedWriter bw = new BufferedWriter(csv);
+
+		while (itr.hasNext()) {
+			try {
+				bw.write(itr.next());
+				bw.newLine();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+
+		csv.close();
 	}
 
 	public static void readFile(String dir) {
